94befe8e693b42786fbcd5ec66f504cf37ff174e
M. Stellmann <matthias.stellmann@haw-hamburg.de>
2013-12-02 10:27:36 +0100

    initial commit

--- src/tcp_advanced/Client.java
+++ src/tcp_advanced/Client.java
@@ -11,22 +11,22 @@ public class Client {
 	private Socket MySocket;
 	private BufferedReader In;
 	private OutputStream Out;
-	
+
 	public Client(String host, int port) throws UnknownHostException, IOException {
 		MySocket = new Socket(host, port);
-		
+
 		In = new BufferedReader(new InputStreamReader(MySocket.getInputStream()));
 		Out = MySocket.getOutputStream();
 	}
-	
+
 	public String receive() throws IOException {
 		return In.readLine();
 	}
-	
+
 	public void send(String message) throws IOException {
 		Out.write((message + "\n").getBytes());
 	}
-	
+
 	public void close() throws IOException {
 		In.close();
 		Out.close();
@@ -35,17 +35,17 @@ public class Client {
 
 	/**
 	 * @param args
-	 * @throws IOException 
-	 * @throws UnknownHostException 
+	 * @throws IOException
+	 * @throws UnknownHostException
 	 */
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		// Verbindung aufbauen
 		Client myClient = new Client("localhost", 14001);
-		
+
 		// Kommunikation
 		myClient.send("Knock, knock!");
 		System.out.println(myClient.receive());
-		
+
 		// Verbindung schliessen
 		myClient.close();
 	}
